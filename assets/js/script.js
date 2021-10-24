b336c11a23f314d3c44b14ddcc10809f2dd95a98
Evan Burton <butonevan1@gmaail.com>
2021-10-24 16:16:38 -0600

    completed js and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,99 +3,116 @@ const travelImages = [
     id: 1,
     title: 'New York Skyline',
     description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
+    width: '480px',
     height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
+    pathURL: 'image-1.jpg',
     linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
-}
+},
 {
     id: 2,
-    title: 'New York Skyline',
-    description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
+    title: 'The Canals',
+    description: "Look at the sunset over the canals.",
+    width: '480px',
     height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
-    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    pathURL: 'image-2.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Canal',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
-}
+},
 {
     id: 3,
-    title: 'New York Skyline',
-    description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
+    title: 'Santorini',
+    description: "Villas overlooking the sea in Santorini",
+    width: '480px',
     height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
-    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    pathURL: 'image-3.jpg',
+    linkURL: 'hhttps://en.wikipedia.org/wiki/Santorini',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
-}
+},
 {
     id: 4,
-    title: 'New York Skyline',
-    description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
+    title: 'Catacombs',
+    description: "It may look creepy but its quite special!",
+    width: '480px',
     height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
-    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    pathURL: 'image-4.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Catacombs',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
-}
+},
 {
     id: 5,
-    title: 'New York Skyline',
-    description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
+    title: 'Mosque',
+    description: "A Mosque used for prayer, prisitine white walls everywhere!",
+    width: '480px',
     height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
-    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    pathURL: 'image-5.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Mosque',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
-}
+},
 {
     id: 6,
-    title: 'New York Skyline',
-    description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
+    title: 'City Street',
+    description: "Twisting corridors in old Italian city!",
+    width: '480px',
     height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
-    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    pathURL: 'image-6.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Venice',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
-}
+},
 {
     id: 7,
-    title: 'New York Skyline',
-    description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
+    title: 'Big Ben',
+    description: "Big Ben looking out over the city.",
+    width: '480px',
     height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
-    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    pathURL: 'image-7.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Big_Ben',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
-}
+},
 {
     id: 8,
-    title: 'New York Skyline',
-    description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
+    title: 'Parthenon',
+    description: "Old Roman architecture is still around today!",
+    width: '480px',
     height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
-    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    pathURL: 'image-8.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Parthenon',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
-}
+},
 {
     id: 9,
-    title: 'New York Skyline',
-    description: "Skyline of the beautiful big apple. What a view!",
-    width: '300px',
-    height: '300px',
-    pathURL: 'assets/images/image 1.jpg',
-    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    title: 'Chicago',
+    description: "The busy streets of Chicago, always moving!",
+    width: '1000px',
+    height: '600px',
+    pathURL: 'image-9.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/Chicago',
     credit: 'Pixabay',
     creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
 }
-]
+];
+
+
+let output = '';
+
+travelImages.forEach(function(travel){
+    console.log(travel)
+    output += `
+    <figure>
+    <a href="${travel.creditURL}">
+    <img src="assets/images/${travel.pathURL}" alt="${travel.title}" width="${travel.width}" height="${travel.height}">
+    </a><figcaption>${travel.description}</figcaption>
+    </figure>
+    `
+
+});
+const gallery =  document.querySelector('.gallery');
+gallery.innerHTML = output;
