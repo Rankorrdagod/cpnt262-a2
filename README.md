518b20d036b8c89040e1e0c3ae068d2cddcc0cd4
Evan Burton <butonevan1@gmaail.com>
2021-10-24 09:59:21 -0600

    setup assets and dropped in images

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cpnt262-a2
+## Attributions
+https://pixabay.com/photos/city-buildings-architecture-towers-336708/
+https://pixabay.com/photos/canal-venice-italy-water-river-1209808/
+https://pixabay.com/photos/santorini-greece-buildings-houses-416136/
+https://pixabay.com/photos/vaulted-cellar-tunnel-arches-keller-247391/
+https://pixabay.com/photos/mosque-abu-dhabi-to-travel-white-615415/
+https://pixabay.com/photos/street-architecture-city-road-3453557/
+https://pixabay.com/photos/palace-london-parliament-big-ben-530055/
+https://pixabay.com/photos/greece-parthenon-temple-ruins-1594689/
+https://pixabay.com/photos/pedestrians-crossing-traffic-1853552/
---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+const travelImages = [
+{
+    id: 1,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+{
+    id: 2,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+{
+    id: 3,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+{
+    id: 4,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+{
+    id: 5,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+{
+    id: 6,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+{
+    id: 7,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+{
+    id: 8,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+{
+    id: 9,
+    title: 'New York Skyline',
+    description: "Skyline of the beautiful big apple. What a view!",
+    width: '300px',
+    height: '300px',
+    pathURL: 'assets/images/image 1.jpg',
+    linkURL: 'https://en.wikipedia.org/wiki/New_York_City',
+    credit: 'Pixabay',
+    creditURL: 'https://pixabay.com/photos/city-buildings-architecture-towers-336708/'
+}
+]
